5e92884cd8e77ff19e0242d95ab9d15f74835720
NuruMero <adanguerreromesa@gmail.com>
2025-01-14 16:46:43 +0100

    Lesson 1

---
+++ src/HolaMundo.java
@@ -0,0 +1,10 @@
+public class HolaMundo {
+
+    // Agregamos el metodo principal (main)
+    // psvm
+    public static void main(String[] args) {
+        // sout
+        System.out.println("Hola Mundo");
+        System.out.println("Saludos");
+    }
+}
---
+++ src/Presentate.java
@@ -0,0 +1,8 @@
+public class Presentate {
+
+    public static void main(String[] args) {
+        System.out.println("Nombre: Adán Guerrero Mesa");
+        System.out.println("Edad: 23 años");
+        System.out.println("País: España");
+    }
+}
